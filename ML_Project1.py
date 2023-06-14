b8d8bdd566c5a1e8dd3f9d4a9e3cbd90c777e32a
imrankhan4u <136573546+imrankhan4u@users.noreply.github.com>
2023-06-14 19:34:17 +0530

    ML_Project1.py

--- ML_Project1.py
+++ ML_Project1.py
@@ -7,28 +7,26 @@ import streamlit as st
 loaded_model = pickle.load(open('trained_model .sav', 'rb'))
 
 # Creating prediction
-def prediction(input_data):
-    # Converting input data to numeric values
-    input_data = [float(x) for x in input_data]
+def fun_prediction(input_data):
 
     # Changing the input_data to a numpy array
     input_data_as_numpy_array = np.asarray(input_data)
     
-    input_data_as_numpy_array = np.asarray(input_data)
     # reshape the np array as we are predicting for one instance
     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+    
     prediction = loaded_model.predict(input_data_reshaped)
+    
     print(prediction)
-    if (prediction[0]==1):
-        print('Loan is approved')
+    if prediction[0]==1 :
+        return 'Loan is approved'
     else:
-        print('Loan is not approved')
+        return 'Loan is not approved'
         
 def main():
     # Giving title
     st.title('Loan Status Prediction')
     # Getting input data from the user
-    Loan_ID = st.text_input('Loan_ID')
     Gender= st.text_input('Gender')
     Married = st.text_input('Married ')
     Dependents= st.text_input('Dependents')
@@ -46,8 +44,7 @@ def main():
     diagnosis = ""
 
     if st.button("User Result"):
-        input_data = [Loan_ID,Gender,Married,Dependents,Education,Self_Employed,ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,Property_Area]
-        diagnosis = prediction(input_data)
+        diagnosis = fun_prediction([Gender,Married,Dependents,Education,Self_Employed,ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,Property_Area])
 
     st.success(diagnosis)
     
